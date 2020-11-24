8373ca4b0ebdabbca274c425e6af996d0b39cf2c
Tomoki Tennichi <tomoki.ten.chuck@gmail.com>
2020-11-24 14:51:56 +0900

    css files and js files are not refernced by the application, so I need to write css codes in blade.php files probably...

--- php_setting/backend/routes/web.php
+++ php_setting/backend/routes/web.php
@@ -14,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',function(){
-    return view('imageweb');
+    return view('welcome');
 });
+// Route::get('/',function(){
+//     return view('imageweb');
+// });
 
 //Route::get('/', function () {return view('welcome');});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
